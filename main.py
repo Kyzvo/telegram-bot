b9033534887399c4d7587fe88d0b283706560ab1
Kyzvo <pidjakmoz1@gmail.com>
2025-02-19 13:31:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -43,3 +43,29 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Команда /start
+def start(update: Update, context: CallbackContext):
+    # Создаем кнопку
+    keyboard = [[InlineKeyboardButton("Open Mini App", web_app={"url": "https://telegram-bot-1-i7hd.onrender.com"})]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Отправляем сообщение с кнопкой
+    update.message.reply_text("Добро пожаловать! Нажмите кнопку ниже, чтобы открыть мини-приложение.", reply_markup=reply_markup)
+
+# Основная функция
+def main():
+    # Вставьте сюда токен вашего бота
+    updater = Updater("7723248117:AAGPFMPIkUkF3tE4EdR7cl9m1IG0f6fmPEs")
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
